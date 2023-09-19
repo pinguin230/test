269744313c809eac877f78a027e3aa708daac40f
pinguin230 <tesliamain@gmail.com>
2023-09-19 17:21:09 +0300

    Test

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	
+		public static void main(String[] args) {
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("Enter the value of N:");
+				int n = scanner.nextInt();
+		
+				LukeNumber[] lukeNumbers = new LukeNumber[n];
+				long sumOfSquares = 0;
+		
+				for (int i = 1; i <= n; i++) {
+							lukeNumbers[i - 1] = new LukeNumber(i);
+							sumOfSquares += lukeNumbers[i - 1].getSquare();
+					}
+			
+					System.out.println("The sum of squares of first " + n + " Luke numbers is: " + sumOfSquares);
+			
+					scanner.close();
+				}
+			}
+			
+// public class Main {
+// 	public static void main(String[] args) {
+// 			System.out.println("Enter the value of N:");
+// 	}	
+// }
